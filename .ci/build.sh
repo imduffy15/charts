4d5f43b986edcbf4b8db5bf786c752513babaced
Ian Duffy <ian@ianduffy.ie>
2020-07-23 02:31:24 +0100

    Add mosquitto and tasmoadmin charts

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,6 +3,8 @@
 set -ex
 set -o pipefail
 
+CHARTSDIR="$(pwd)/charts"
+
 [ -z "$GITHUB_PAGES_BRANCH" ] && GITHUB_PAGES_BRANCH=gh-pages
 
 [ -z "$HELM_CHARTS_SOURCE" ] && HELM_CHARTS_SOURCE="charts"
@@ -10,17 +12,25 @@ set -o pipefail
 echo "GITHUB_PAGES_BRANCH=$GITHUB_PAGES_BRANCH"
 echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
 
-mkdir gh-pages
-cd gh-pages
+mkdir -p /tmp/gh-pages
 
 echo '>> Building charts...'
-git diff --diff-filter=d --dirstat=files,0 HEAD | awk '{print $2}' | grep -i -E "charts" || true | cut -d/ -f2 | sort | uniq | while read chart; do
+git diff --diff-filter=d --dirstat=files,0 HEAD~1 | awk '{print $2}' | (grep -i -E "charts" || true) | cut -d/ -f2 | sort | uniq | while read chart; do
+  pushd /tmp/gh-pages
+  chart=$CHARTSDIR/$chart
   echo ">>> helm lint $chart"
   helm lint "$chart"
   chart_name="`basename "$chart"`"
   echo ">>> helm package -d $chart_name $chart"
   mkdir -p "$chart_name"
   helm package -d "$chart_name" "$chart"
+  popd
 done
+
+git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch origin
+git checkout -b gh-pages origin/gh-pages
+rsync -a /tmp/gh-pages/ .
+
 echo '>>> helm repo index'
 helm repo index .
--- .ci/setup.sh
+++ .ci/setup.sh
@@ -7,14 +7,13 @@ set -o pipefail
 
 echo "HELM_VERSION=$HELM_VERSION"
 
-echo $PATH
-
 echo '>> Installing Helm...'
-mkdir -p /tmp/helm/bin
-cd /tmp/helm/bin
+mkdir -p /tmp/helm
+pushd /tmp/helm
 wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 chmod +x linux-amd64/helm
 mv linux-amd64/helm /home/travis/bin/helm
 helm version -c
 helm init -c
+popd
