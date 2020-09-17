3f5cefd5b87f759a290e0034c71dee9f27e46f63
Ian Duffy <ian@ianduffy.ie>
2020-09-17 23:14:45 +0100

    Add cert manager from https://charts.jetstack.io/charts/cert-manager-v1.0.1.tgz to workaround their invalid helm repo

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -3,17 +3,16 @@
 set -e
 set -o pipefail
 
-[ -z "$HELM_VERSION" ] && HELM_VERSION=2.16.0
+[ -z "$HELM_VERSION" ] && HELM_VERSION=3.3.2
 
 echo "HELM_VERSION=$HELM_VERSION"
 
 echo '>> Installing Helm...'
 mkdir -p /tmp/helm
 pushd /tmp/helm
-wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+wget "https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 chmod +x linux-amd64/helm
 mv linux-amd64/helm /home/travis/bin/helm
 helm version -c
-helm init -c
 popd
